1548b0e008d34c16c74886fd19950a9092874542
stevensam0109 <steven.sam.0109@outlook.com>
2021-06-01 20:50:03 +0900

    ADD cardBelongingCardlist subscriber

--- mytrello-back/src/subscriber/belongingRequired.js
+++ mytrello-back/src/subscriber/belongingRequired.js
@@ -26,6 +26,31 @@ async function cardlistBelongingBoardRequired(req, res, next) {
   }
 }
 
+/**
+ * Check if the card is belonging to the cardlist
+ */
+async function cardBelongingCardlistRequired(req, res, next) {
+  try {
+    const cardlist = await db.cardlist.findByPk(req.params.cardlistId);
+    const card = await db.card.findByPk(req.params.cardId);
+
+    if (cardlist === null)
+      return { statusCode: HTTP.NotFound, data: "cardlist not found" };
+    if (card === null)
+      return { statusCode: HTTP.NotFound, data: "card not found" };
+
+    if (!(await cardlist.hasCard(card))) {
+      return res
+        .status(HTTP.Forbidden)
+        .send("The card is not belonging to the cardlist");
+    }
+    return next();
+  } catch (err) {
+    return res.status(HTTP.InternalServerError).send({ data: err.message });
+  }
+}
+
 module.exports = {
   cardlistBelongingBoardRequired,
+  cardBelongingCardlistRequired,
 };
